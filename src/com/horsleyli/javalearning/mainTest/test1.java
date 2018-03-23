85d2dffceac2874aa23b88b550ef69a1c8d48a6c
Ji2846 <2846257141@qq.com>
2018-03-24 00:58:24 +0800

    hi

--- src/com/horsleyli/javalearning/mainTest/test1.java
+++ src/com/horsleyli/javalearning/mainTest/test1.java
@@ -3,5 +3,6 @@ package com.horsleyli.javalearning.mainTest;
 public class test1{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Hi");
     }
 }
