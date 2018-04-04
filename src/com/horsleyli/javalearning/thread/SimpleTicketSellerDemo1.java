fc02255f70caf765efd627ad0df8165e14ab1270
Ji2846 <2846257141@qq.com>
2018-04-05 04:09:16 +0800

    hi

---
+++ src/com/horsleyli/javalearning/thread/SimpleTicketSellerDemo1.java
@@ -0,0 +1,48 @@
+package com.horsleyli.javalearning.thread;
+
+public class SimpleTicketSellerDemo1 {
+    final static int TICKETS = 300;
+
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new TicketSeller1("甲"));
+        Thread t2 = new Thread(new TicketSeller1("乙"));
+        Thread t3 = new Thread(new TicketSeller1("丙"));
+
+        System.out.println("总票数: " + TICKETS);
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
+
+class TicketSeller1 implements Runnable {
+    private String name;
+
+    TicketSeller1(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        sellTickets();
+        System.out.println(name + ": 一共卖出了" + soldTickets + "张票");
+    }
+
+    int soldTickets = 0;
+    static int tickets = SimpleTicketSellerDemo1.TICKETS;
+
+    private boolean checkTicketCount() {
+        if (tickets > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private void sellTickets() {
+        while (checkTicketCount()) {
+            tickets--;
+            System.out.println(name + ": 卖出了1张票，还剩下" + tickets + "张");
+            soldTickets++;
+        }
+    }
+}
