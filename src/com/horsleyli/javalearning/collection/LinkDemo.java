5f6b8f9dd0dbf4a1dca01996eb586bd0407aa400
Ji2846 <2846257141@qq.com>
2018-04-20 22:58:58 +0800

    Update LinkDemo.java

--- src/com/horsleyli/javalearning/collection/LinkDemo.java
+++ src/com/horsleyli/javalearning/collection/LinkDemo.java
@@ -3,27 +3,24 @@ package com.horsleyli.javalearning.collection;
 public class LinkDemo {
     // 链表
     public static void main(String[] args) {
-        Node head = new Node("链表头");
+        go();
+    }
+
+    private static void go() {
+        Node root = new Node("链表头");
         Node n1 = new Node("1号格");
         Node n2 = new Node("2号格");
         Node n3 = new Node("3号格");
         Node end = new Node("链表尾");
 
-        head.setNext(n1);
-        n1.setNext(n2);
-        n2.setNext(n3);
-        n3.setNext(end);
+        Link link = new Link();
+        link.add(root);
+        link.add(n1);
+        link.add(n2);
+        link.add(n3);
+        link.add(end);
 
-        print(head);
-    }
-
-    public static void print(Node current) {
-        // 递归操作
-        while (current != null) {
-            // 当current不为空时，输出数据，然后将current设为next
-            System.out.println(current.getData());
-            current = current.getNext();
-        }
+        link.print();
     }
 }
 
@@ -35,15 +32,38 @@ class Node {
         this.data = data;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
+    void addNode(Node newNode) {
+        if (this.next == null) {
+            this.next = newNode;
+        } else {
+            this.next.addNode(newNode);
+        }
     }
 
-    public Node getNext() {
-        return next;
+    void printNode() {
+        System.out.println(this.data);
+        if (this.next != null) {
+            this.next.printNode();
+        }
+    }
+}
+
+class Link {    // 负责元素操作的类
+    private Node root;    // 根节点
+
+    // 添加元素
+    void add(Node newNode) {
+        if (root == null) {
+            this.root = newNode;
+        } else {
+            root.addNode(newNode);
+        }
     }
 
-    public String getData() {
-        return data;
+    // 输出数据
+    void print() {
+        if (this.root != null) {
+            this.root.printNode();
+        }
     }
 }
